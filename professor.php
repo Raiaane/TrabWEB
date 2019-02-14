405c401611fbb7584e67fd227a1c363766266900
raiaane <costaraiane25@gmail.com>
2019-02-14 12:10:55 -0200

    Cadastro aluno na pagina inicial

---
+++ professor.php
@@ -0,0 +1,93 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<style type="text/css">
+	
+	* {
+		padding: 0 auto;
+		margin: 0;
+	}
+
+	.container {
+		width: 100%;
+		height: 60px;
+		background: #48D1CC;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.teste ul li {
+		display: inline;
+		text-decoration: none;
+		padding: 0px 30px;
+		margin-left: 30px;
+	}
+
+	.teste ul li a {
+		color: #fff; 
+		text-decoration: none;
+		
+	}
+	.flex-container{
+		display: flex;
+		justify-content: center;
+		background-color: white;
+		width: 300px;
+		height: 440px;
+		margin-left: 240px;
+		margin-top: 60px;
+}
+	
+
+  
+
+		
+  
+
+
+
+
+    
+
+
+	
+
+</style>
+<body background="images/fundo4.jpg">
+
+<!-- menu #########################################################################################################################################################-->
+	<div class="container">
+
+		<nav><h2  style="margin-left: -30px; color: white; font-family: verdana; "> Simulado online</h2> </nav>
+		<nav><img class="logo" src="images/icon2.png" style="padding-top: -20px; margin-left: -290px; width: 60px;"></nav>
+		<nav class="teste">
+
+			<ul>
+
+				<li>
+					<a href="" style="font-family: verdana;">PROFESSOR</a>
+				</li>
+				<li>
+					<a href="" style="font-family: verdana;">DICAS</a>
+				</li>
+				<li>
+					<a href="" style="font-family: verdana;">SOBRE</a>
+				</li>
+				<li>
+					<a href="menu.php" style="font-family: verdana; ">SAIR</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+
+	<!-- cadastro ################################################################################################################################################-->
+	
+ <!--login ###################################################################################################################################################-->
+ 	    
+
+</body>
+</html>
